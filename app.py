771cded500e65a7a61a01096848ffba3e7b40707
Tia Hwang <tiajshwang@gmail.com>
2020-04-19 20:30:30 +0900

    email별로 등록된 habit과 username 로딩

--- app.py
+++ app.py
@@ -9,19 +9,35 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbhabits
 
+# global 변수선언
+current_email = ''
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/habits', methods=['GET'])
 def listing():
-    result = list(db.habits.find({}, {'_id': 0}))
-    return render_template("habits.html", habits=result)
+    global current_email
+    habit_list = list(db.habits.find({}, {'_id': 0}))
+    print(habit_list)
+    user_habit_list = []
+    for habit in habit_list:
+        if habit['email'] == current_email:
+            user_habit_list.append(habit)
+    username=current_email.split("@")[0]
+    return render_template("habits.html", email=username, habits=user_habit_list)
 
 @app.route('/habits', methods=['POST'])
 def saving():
+    global current_email
     email_receive = request.form['email']
     habits_receive = request.form.getlist('habits[]')
+
+    # save current email
+    current_email = email_receive
+
+    # email_receive를 listing 에서 참조해야함
 
     for h in habits_receive:
         if str(h).strip() == "":
@@ -30,6 +46,7 @@ def saving():
         db.habits.insert_one(habit)
 
     return jsonify({'result': 'success'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
